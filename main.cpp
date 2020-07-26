d988fe12f6391a142c846e41141fe8f885900845
Tima <onlyosuplay@gmail.com>
2020-07-27 02:25:14 +0300

    added catch in main

--- main.cpp
+++ main.cpp
@@ -25,34 +25,44 @@ std::ostream& operator<<(std::ostream& out, const point& p) {
 	return out;
 }
 
-
 int main() {
-	matrix<int> m_int(3, 3);
-	matrix<double> m_double(3, 3);
-	matrix<char> m_char(3, 3);
-	matrix<point> m_point(3, 3);
+	try {
+		matrix<int> m_int(3, 3);
+		matrix<double> m_double(3, 3);
+		matrix<char> m_char(3, 3);
+		matrix<point> m_point(3, 3);
 
-	m_int.set1(9);
-	m_double.set1(3.14);
-	m_char.set1('A');
-	m_point.set1(5);
+		m_int.set1(9);
+		m_double.set1(3.14);
+		m_char.set1('A');
+		m_point.set1(5);
 
-	std::cout << m_int << std::endl;
-	std::cout << m_double << std::endl;
-	std::cout << m_char << std::endl;
-	std::cout << m_point << std::endl;
+		std::cout << m_int << std::endl;
+		std::cout << m_double << std::endl;
+		std::cout << m_char << std::endl;
+		std::cout << m_point << std::endl;
 
-	std::cout << "===================================================" << std::endl << std::endl;
+		std::cout << "===================================================" << std::endl << std::endl;
 
-	m_int = m_int + m_int;
-	m_double = m_double + m_double;
-	m_char = m_char + m_char;
-	m_point = m_point + m_point;
-	
-	std::cout << m_int << std::endl;
-	std::cout << m_double << std::endl;
-	std::cout << m_char << std::endl;
-	std::cout << m_point << std::endl;
+		m_int = m_int + m_int;
+		m_double = m_double + m_double;
+		m_char = m_char + m_char;
+		m_point = m_point + m_point;
+
+		std::cout << m_int << std::endl;
+		std::cout << m_double << std::endl;
+		std::cout << m_char << std::endl;
+		std::cout << m_point << std::endl;
+	}
+	catch (std::bad_alloc& e) {
+		std::cerr << e.what();
+	}
+	catch (std::out_of_range& e) {
+		std::cerr << e.what();
+	}
+	catch (std::logic_error& e) {
+		std::cerr << e.what();
+	}
 
 	return 0;
 }
