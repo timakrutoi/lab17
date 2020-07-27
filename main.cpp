c35ac531c02d4e6521d54c3d627fd5f0d7183468
Tima <onlyosuplay@gmail.com>
2020-07-27 18:06:22 +0300

    added func for tests

--- main.cpp
+++ main.cpp
@@ -25,6 +25,15 @@ std::ostream& operator<<(std::ostream& out, const point& p) {
 	return out;
 }
 
+template<typename T>
+void test_(matrix<T>& m, T arg) {
+	m.set1(arg);
+	std::cout << m << std::endl;
+	m = m + m;
+	std::cout << m << std::endl;
+	std::cout << "===================================================" << std::endl << std::endl;
+}
+
 int main() {
 	try {
 		matrix<int> m_int(3, 3);
@@ -32,35 +41,12 @@ int main() {
 		matrix<char> m_char(3, 3);
 		matrix<point> m_point(3, 3);
 
-		m_int.set1(9);
-		m_double.set1(3.14);
-		m_char.set1('A');
-		m_point.set1(5);
-
-		std::cout << m_int << std::endl;
-		std::cout << m_double << std::endl;
-		std::cout << m_char << std::endl;
-		std::cout << m_point << std::endl;
-
-		std::cout << "===================================================" << std::endl << std::endl;
-
-		m_int = m_int + m_int;
-		m_double = m_double + m_double;
-		m_char = m_char + m_char;
-		m_point = m_point + m_point;
-
-		std::cout << m_int << std::endl;
-		std::cout << m_double << std::endl;
-		std::cout << m_char << std::endl;
-		std::cout << m_point << std::endl;
+		test_(m_int, 9);
+		test_(m_double, 3.14);
+		test_(m_char, 'A');
+		test_(m_point, point(3));
 	}
-	catch (std::bad_alloc& e) {
-		std::cerr << "Error: " << e.what();
-	}
-	catch (std::out_of_range& e) {
-		std::cerr << "Error: " << e.what();
-	}
-	catch (std::logic_error& e) {
+	catch (std::exception& e) {
 		std::cerr << "Error: " << e.what();
 	}
 
